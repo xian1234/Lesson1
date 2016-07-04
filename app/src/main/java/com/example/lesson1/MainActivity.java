616f677303d7d66db02c5390f4708ae7e7e62c38
unknown <>
2016-07-04 13:01:54 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lesson1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.lesson1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void display(View view){
+        Toast.makeText(this,"This will launch your app", Toast.LENGTH_SHORT).show();
+    }
+}
